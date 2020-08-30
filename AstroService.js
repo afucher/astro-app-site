c75117b349d7dc62b3397c07d79d900819272156
Arthur Fücher <arthurfucher@gmail.com>
2020-08-30 18:03:07 -0300

    adiciona cidades

--- AstroService.js
+++ AstroService.js
@@ -1,6 +1,19 @@
 var AstroService = {};
 
-AstroService.ObterAstros = function(date) {
-    return fetch(`https://astroz.herokuapp.com/Astros?data=${date}&lat=-23.533773&long=-46.625290`)
+AstroService.ObterAstros = function(date, local) {
+    const [lat, long] = ObterLatitudeLongitude(local);
+    return fetch(`https://astroz.herokuapp.com/Astros?data=${date}&lat=${lat}&long=${long}`)
     .then((response) => response.json())
 };
+
+function ObterLatitudeLongitude(local) {
+    switch(local) {
+        case 'rj':
+            return [-22.9035, -43.2096]
+        case 'cuiaba':
+            return [-15.5989, -56.0949]
+        case 'sp':
+        default: 
+            return [-23.533773, -46.625290]
+    }
+}
