f19731c44cf46e94c916a6ca300dece4f5369b72
Arthur Fücher <arthurfucher@gmail.com>
2020-08-30 15:19:48 -0300

    troca url para https

--- AstroService.js
+++ AstroService.js
@@ -2,6 +2,6 @@ var AstroService = {};
 
 AstroService.ObterAstros = function() {
     const date = new Date().toISOString();
-    return fetch(`http://astroz.herokuapp.com/Astros?data=${date}&lat=-23.533773&long=-46.625290`)
+    return fetch(`https://astroz.herokuapp.com/Astros?data=${date}&lat=-23.533773&long=-46.625290`)
     .then((response) => response.json())
 };
